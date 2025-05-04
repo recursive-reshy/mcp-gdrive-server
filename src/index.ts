bf2b076bb01ffffc0e78db94ca06ffa7ba790f56
recursive-reshy <61969021+recursive-reshy@users.noreply.github.com>
2025-05-04 17:39:05 +0800

    src\index.ts: Load env variables

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 #!/usr/bin/env node
-
+import 'dotenv/config'
 import { StdioServerTransport } from '@modelcontextprotocol/sdk/server/stdio.js'
 
 import Server from "./server.js"
@@ -7,7 +7,7 @@ import Server from "./server.js"
 const main = async () => {
 
   try {
-    console.log('Starting server...')
+    console.log('Starting server...', process.env.CLIENT_SECRET)
     await Server.connect( new StdioServerTransport() )
     console.log('Server started')
   } catch (error) {
